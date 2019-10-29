be7136d8bc3a8692c2f0bd8c3f487d74da5e6d03
StiviiK <stefan.kuerzeder@gmail.com>
2019-10-29 12:56:33 +0100

    Removed unused stuff

--- resources/download-resources.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-BASE_URL=$(jq "._baseUrl" urls.json)
-echo $BASE_URL
-jq '.stable | to_entries[] | .key' urls.json  | while read i; do
-    TEST=$(jq ".stable.${i}" urls.json)
-    echo "${BASE_URL}${TEST}"
-done
